2fdae97341d28fa3b470baafdf5e6fda48e6d3ea
Kendall Agbulos <Jarvis@Kendalls-MacBook-Pro.local>
2014-07-12 14:53:46 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,22 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Kendall Agbulos on 7/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+-(id) init{
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"PENGUIN CREATED");
+    }
+    
+    return self;
+}
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,22 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Kendall Agbulos on 7/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+-(id) init{
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"SEAL CREATED");
+    }
+    
+    return self;
+}
+
+@end
