c413c550f3cb61515cc83f883d2c49a352a1206e
Kendall Agbulos <Jarvis@Kendalls-MacBook-Pro.local>
2014-07-24 16:40:20 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Kendall Agbulos on 7/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
